8043c4f090061bd39f0c15f5fab2f58057953a8d
Gabe Hassler <ghassler@rand.org>
2024-11-19 22:49:26 -0800

    starting server examples

--- examples/test.py
+++ examples/test.py
@@ -1,48 +1,11 @@
-import numpy as np
-import time
-# from multiprocessing import Pool
-from pathos.multiprocessing import ProcessingPool as Pool
+import torch
 
-def rdet(n):
-    X = np.random.normal(size=(n, n))
-    X = np.dot(X.T, X)
-    return np.log(np.linalg.det(X))
-
-iterations = 1000
-ns = range(1, 51)
-
-# Serial computation
-mean_dets1 = np.zeros(len(ns))
-t1 = time.time()
-for i in range(len(ns)):
-    s = 0
-    for j in range(iterations):
-        s += rdet(ns[i])
-    mean_dets1[i] = s / iterations
-
-t2 = time.time()
-
-# Parallel computation
-# def parallel_rdet(iterations):
-#     for j in range(iterations):
-#         s += rdet(n)
-#     return s / iterations
-
-# if __name__ == '__main__':
-t3 = time.time()
-
-for n in ns:
-    mean_det =0
-
-    with Pool(processes=8) as pool:
-        dets = pool.map(rdet, iterations)
-        mean_det = np.mean(dets)
-    print(mean_det)
-t4 = time.time()
-
-print(f"Serial time: {t2 - t1}")
-# print(f"Serial time (list comprehension): {t2s - t1s}")
-print(f"Parallel time: {t4 - t3}")
-
-import pandas as pd
-df = pd.DataFrame({'n': ns, 'serial': mean_dets1, 'parallel': mean_dets2})
+# Get cpu, gpu or mps device for training.
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+print(f"Using {device} device")
